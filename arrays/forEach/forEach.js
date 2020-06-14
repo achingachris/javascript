abb16f11ff13ced53f108f8132d7f296a1db3adb
ChrisAchinga <achinga.chris@gmail.com>
2020-06-14 19:10:12 +0300

    using forEach() in arrays

---
+++ arrays/forEach/forEach.js
@@ -0,0 +1,9 @@
+// The forEach() method executes a provided function once for each array element
+
+// does not wait or promises
+
+
+// example
+const langauges = ['Java', 'JavaScript', 'PHP', 'Python']
+
+displayLang = langauges.forEach(lang => console.log(`This is ${lang}`));
