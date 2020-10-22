9a88a08057fe2bd11b5b7f89372902658189894b
ChrisAchinga <achinga.chris@gmail.com>
2020-10-22 13:42:33 +0300

    updated

---
+++ src/basic_js/count-backwards-with-a-for-loop.js
@@ -0,0 +1,7 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
---
+++ src/basic_js/iterate-odd-numbers-with-a-for-loop.js
@@ -0,0 +1,7 @@
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
