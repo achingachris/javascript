9bfb0e1ce04b82ba2e3d6efb8337524e05a213cc
ChrisAchinga <achinga.chris@gmail.com>
2020-10-30 12:14:34 +0300

    new

---
+++ ES6/write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,7 @@
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
---
+++ ES6/write-concise-object-literal-declarations-using-object-property-shorthand.js
@@ -0,0 +1,10 @@
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // change code above this line
+};
