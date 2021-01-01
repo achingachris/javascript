87f5c5e0830b6aa4a007fbbe00c7a20610cbe45c
ChrisAchinga <achinga.chris@gmail.com>
2021-01-01 13:00:37 +0300

    hackerrank challenges

---
+++ hacker_rank/challenge1.js
@@ -0,0 +1,17 @@
+function greetings(text) {
+    console.log('Hello, World!')
+    console.log(text)
+}
+
+greetings('Welcome to 10 Days of JavaScript!');
+
+function main() {
+    // Get a line of input from stdin and save it to our variable
+    var inputString = readLine();
+
+    // Your first line of output goes here
+    console.log("Hello World");
+
+    // Write the second line of outpu
+    console.log(inputString);
+}
